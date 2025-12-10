30f9a7d430e0e72d0a226f05cece29df23c4f793
ahistah <ahmed.is.tah@gmail.com>
2025-12-10 16:00:45 +0500

    New comp

--- Codeforces Round 1069 (Div. 2)/B. XOR Array.py
+++ Codeforces Round 1069 (Div. 2)/B. XOR Array.py
@@ -1,9 +1,10 @@
 t = int(input())
 for _ in range(t):
     n, l, r = map(int, input().split())
+    arrayy = []
     previous = 0
     for i in range(1, n + 1):
-        current = l - 1 if i == r else i
-        a.append(current ^ previous)
+        current  = l - 1 if i == r else i
+        arrayy.append(current ^ previous)
         previous = current
-    print(*a)
+    print(*arrayy)
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # codeforces
 
 Having fun climbing the leaderboard in competitive programming!
+
+Currently top 200 in Pakistan!
